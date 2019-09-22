366cc4cc44c2ae2be5bf7f5d914b24bf5e5325ae
Claudia Saxer <c.s.saxer@gmail.com>
2019-09-22 22:29:45 +0200

    add error message on exception

--- ExcelParserExt/ExcelParserExt/MainWindow.xaml.cs
+++ ExcelParserExt/ExcelParserExt/MainWindow.xaml.cs
@@ -28,59 +28,66 @@ namespace ExcelParserExt
 
         #endregion
 
-        void OpenFileButton_OnClick( Object sender, RoutedEventArgs e )
+        private void OpenFileButton_OnClick(object sender, RoutedEventArgs e)
         {
             var ofd = new OpenFileDialog();
-            if ( ofd.ShowDialog() == true )
+            if (ofd.ShowDialog() == true)
             {
                 FilePath.Text = ofd.FileName;
-                ReadFile( ofd.FileName );
+                try
+                {
+                    ReadFile(ofd.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), $"Fuck!: {ex.Message}", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
         }
 
-        private DataSet Parse( String fileName, String sheetName )
+        private DataSet Parse(string fileName, string sheetName)
         {
             var connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={fileName};Extended Properties=Excel 12.0;";
 
             var data = new DataSet();
-            using ( var con = new OleDbConnection( connectionString ) )
+            using (var con = new OleDbConnection(connectionString))
             {
                 var dataTable = new DataTable();
-                var query = "SELECT * FROM [{0}]".F( sheetName );
+                var query = "SELECT * FROM [{0}]".F(sheetName);
                 con.Open();
-                var adapter = new OleDbDataAdapter( query, con );
-                adapter.Fill( dataTable );
-                data.Tables.Add( dataTable );
+                var adapter = new OleDbDataAdapter(query, con);
+                adapter.Fill(dataTable);
+                data.Tables.Add(dataTable);
             }
 
             return data;
         }
 
-        private void ReadFile( String path )
+        private void ReadFile(string path)
         {
-            var data = Parse( path, SheetName.Text );
-            var result = ( from DataRow row in data.Tables[0].Rows
-                           select new
-                           {
-                               url = row[0],
-                               url_min = row[1],
-                               size = row[8],
-                               sold = row[9].ToString() == "true",
-                               de = new
-                               {
-                                   title = row[2],
-                                   desc = row[3],
-                                   caption = row[4]
-                               },
-                               en = new
-                               {
-                                   title = row[5],
-                                   desc = row[6],
-                                   caption = row[7]
-                               }
-                           } ).Cast<Object>()
-                              .ToList();
-            var res = JsonConvert.SerializeObject( result, Formatting.Indented );
+            var data = Parse(path, SheetName.Text);
+            var result = (from DataRow row in data.Tables[0].Rows
+                    select new
+                    {
+                        url = row[0],
+                        url_min = row[1],
+                        size = row[8],
+                        sold = row[9] as String == "true",
+                        de = new
+                        {
+                            title = row[2],
+                            desc = row[3],
+                            caption = row[4]
+                        },
+                        en = new
+                        {
+                            title = row[5],
+                            desc = row[6],
+                            caption = row[7]
+                        }
+                    }).Cast<object>()
+                .ToList();
+            var res = JsonConvert.SerializeObject(result, Formatting.Indented);
             Result.Text = res;
         }
     }
